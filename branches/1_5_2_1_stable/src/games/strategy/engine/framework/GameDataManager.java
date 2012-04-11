e6952c54165e6608629056fe2ee73d096d855334
veqryn <veqryn@a4c711aa-641f-0410-879d-9b464e52b682>
2012-04-11 16:31:12 +0000

    branching stable (veqryn)

---
+++ branches/1_5_2_1_stable/src/games/strategy/engine/framework/GameDataManager.java
@@ -0,0 +1,180 @@
+package games.strategy.engine.framework;
+
+import games.strategy.engine.EngineVersion;
+import games.strategy.engine.data.GameData;
+import games.strategy.engine.delegate.IDelegate;
+import games.strategy.util.Version;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * <p>
+ * Title: TripleA
+ * </p>
+ * <p>
+ * Description: Responsible for loading saved games, new games from xml, and saving games
+ * </p>
+ * 
+ * @author Sean Bridges
+ */
+public class GameDataManager
+{
+	private final static String DELEGATE_START = "<DelegateStart>";
+	private final static String DELEGATE_DATA_NEXT = "<DelegateData>";
+	private final static String DELEGATE_LIST_END = "<EndDelegateList>";
+	
+	public GameDataManager()
+	{
+	}
+	
+	public GameData loadGame(final File savedGameFile) throws IOException
+	{
+		InputStream input = null;
+		try
+		{
+			input = new BufferedInputStream(new FileInputStream(savedGameFile));
+			return loadGame(input);
+		} finally
+		{
+			try
+			{
+				if (input != null)
+					input.close();
+			} catch (final Exception e)
+			{
+			}
+		}
+	}
+	
+	public GameData loadGame(final InputStream input) throws IOException
+	{
+		return loadGame(new ObjectInputStream(new GZIPInputStream(input)));
+	}
+	
+	public GameData loadGame(final ObjectInputStream input) throws IOException
+	{
+		try
+		{
+			// TODO we should check the game version as well
+			final Version readVersion = (Version) input.readObject();
+			if (!readVersion.equals(EngineVersion.VERSION))
+				throw new IOException("Incompatible engine versions. We are: " + EngineVersion.VERSION + " . Trying to load game created with: " + readVersion);
+			final GameData data = (GameData) input.readObject();
+			loadDelegates(input, data);
+			data.postDeSerialize();
+			return data;
+		} catch (final ClassNotFoundException cnfe)
+		{
+			throw new IOException(cnfe.getMessage());
+		}
+	}
+	
+	private void loadDelegates(final ObjectInputStream input, final GameData data) throws ClassNotFoundException, IOException
+	{
+		for (Object endMarker = input.readObject(); !endMarker.equals(DELEGATE_LIST_END); endMarker = input.readObject())
+		{
+			final String name = (String) input.readObject();
+			final String displayName = (String) input.readObject();
+			final String className = (String) input.readObject();
+			IDelegate instance;
+			try
+			{
+				instance = (IDelegate) Class.forName(className).newInstance();
+				instance.initialize(name, displayName);
+				data.getDelegateList().addDelegate(instance);
+			} catch (final Exception e)
+			{
+				e.printStackTrace();
+				throw new IOException(e.getMessage());
+			}
+			final String next = (String) input.readObject();
+			if (next.equals(DELEGATE_DATA_NEXT))
+			{
+				instance.loadState((Serializable) input.readObject());
+			}
+		}
+	}
+	
+	public void saveGame(final File destination, final GameData data) throws IOException
+	{
+		BufferedOutputStream out = null;
+		try
+		{
+			final OutputStream fileStream = new FileOutputStream(destination);
+			out = new BufferedOutputStream(fileStream);
+			saveGame(fileStream, data);
+		} finally
+		{
+			try
+			{
+				if (out != null)
+					out.close();
+			} catch (final Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void saveGame(final OutputStream sink, final GameData data) throws IOException
+	{
+		saveGame(sink, data, true);
+	}
+	
+	public void saveGame(final OutputStream sink, final GameData data, final boolean saveDelegateInfo) throws IOException
+	{
+		// write internally first in case of error
+		final ByteArrayOutputStream bytes = new ByteArrayOutputStream(25000);
+		final ObjectOutputStream outStream = new ObjectOutputStream(bytes);
+		outStream.writeObject(games.strategy.engine.EngineVersion.VERSION);
+		data.acquireReadLock();
+		try
+		{
+			outStream.writeObject(data);
+			if (saveDelegateInfo)
+				writeDelegates(data, outStream);
+			else
+				outStream.writeObject(DELEGATE_LIST_END);
+		} finally
+		{
+			data.releaseReadLock();
+		}
+		final GZIPOutputStream zippedOut = new GZIPOutputStream(sink);
+		// now write to file
+		zippedOut.write(bytes.toByteArray());
+		zippedOut.flush();
+		zippedOut.close();
+	}
+	
+	private void writeDelegates(final GameData data, final ObjectOutputStream out) throws IOException
+	{
+		final Iterator<IDelegate> iter = data.getDelegateList().iterator();
+		while (iter.hasNext())
+		{
+			out.writeObject(DELEGATE_START);
+			final IDelegate delegate = iter.next();
+			// write out the delegate info
+			out.writeObject(delegate.getName());
+			out.writeObject(delegate.getDisplayName());
+			out.writeObject(delegate.getClass().getName());
+			out.writeObject(DELEGATE_DATA_NEXT);
+			out.writeObject(delegate.saveState());
+		}
+		// mark end of delegate section
+		out.writeObject(DELEGATE_LIST_END);
+	}
+}
---
+++ branches/1_5_2_1_stable/src/games/strategy/triplea/delegate/UndoablePlacement.java
@@ -0,0 +1,113 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package games.strategy.triplea.delegate;
+
+import games.strategy.engine.data.CompositeChange;
+import games.strategy.engine.data.GameData;
+import games.strategy.engine.data.PlayerID;
+import games.strategy.engine.data.Territory;
+import games.strategy.engine.data.Unit;
+import games.strategy.engine.delegate.IDelegateBridge;
+import games.strategy.triplea.delegate.dataObjects.PlacementDescription;
+import games.strategy.triplea.formatter.MyFormatter;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Contains all the data to describe a placement and to undo it.
+ * 
+ * @author Erik von der Osten
+ */
+@SuppressWarnings("serial")
+class UndoablePlacement extends AbstractUndoableMove
+{
+	final Territory m_placeTerritory;
+	Territory m_producerTerritory;
+	PlayerID m_player;
+	
+	public UndoablePlacement(final PlayerID player, final CompositeChange change, final Territory producer_territory, final Territory place_territory, final Collection<Unit> units)
+	{
+		super(change, units);
+		m_placeTerritory = place_territory;
+		m_producerTerritory = producer_territory;
+		m_player = player;
+	}
+	
+	public Territory getProducerTerritory()
+	{
+		return m_producerTerritory;
+	}
+	
+	public void setProducerTerritory(final Territory producerTerritory)
+	{
+		m_producerTerritory = producerTerritory;
+	}
+	
+	public Territory getPlaceTerritory()
+	{
+		return m_placeTerritory;
+	}
+	
+	@Override
+	protected final void undoSpecific(final IDelegateBridge bridge)
+	{
+		final GameData data = bridge.getData();
+		final String currentStepName = bridge.getStepName(); // TODO: currently there is no good way to figure out what Delegate and Step we are currently in
+		AbstractPlaceDelegate currentDelegate;
+		if (currentStepName.endsWith("BidPlace"))
+			currentDelegate = DelegateFinder.bidPlaceDelegate(data);
+		else if (currentStepName.endsWith("Place"))
+			currentDelegate = DelegateFinder.placeDelegate(data);
+		else
+			throw new IllegalStateException("Can not find Placement Delegate for current step: " + currentStepName);
+		final Map<Territory, Collection<Unit>> produced = currentDelegate.getProduced();
+		final Collection<Unit> units = produced.get(m_producerTerritory);
+		units.removeAll(getUnits());
+		if (units.isEmpty())
+		{
+			produced.remove(m_producerTerritory);
+		}
+		currentDelegate.setProduced(new HashMap<Territory, Collection<Unit>>(produced));
+	}
+	
+	@Override
+	public final String getMoveLabel()
+	{
+		if (m_producerTerritory != m_placeTerritory)
+			return m_producerTerritory.getName() + " -> " + m_placeTerritory.getName();
+		return m_placeTerritory.getName();
+	}
+	
+	@Override
+	public final Territory getEnd()
+	{
+		return m_placeTerritory;
+	}
+	
+	@Override
+	protected final PlacementDescription getDescriptionObject()
+	{
+		return new PlacementDescription(m_units, m_placeTerritory);
+	}
+	
+	@Override
+	public String toString()
+	{
+		if (m_producerTerritory != m_placeTerritory)
+			return m_producerTerritory.getName() + " produces in " + m_placeTerritory.getName() + ": " + MyFormatter.unitsToTextNoOwner(m_units);
+		return m_placeTerritory.getName() + ": " + MyFormatter.unitsToTextNoOwner(m_units);
+	}
+}
