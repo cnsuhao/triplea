f2dcd88b5f8312e01de3c60fae19202ad4940237
veqryn <veqryn@a4c711aa-641f-0410-879d-9b464e52b682>
2015-07-27 08:54:01 +0000

    Removing unneeded tags/branches

--- branches/1_7_0_1_unstable/run-server.sh
+++
@@ -1,2 +0,0 @@
-cd $(dirname $0)
-java -server  -Xmx192m -classpath bin/triplea.jar:lib/derby_10_1_2.jar -Dtriplea.lobby.port=3303 -Dtriplea.lobby.console=true  games.strategy.engine.lobby.server.LobbyServer 
